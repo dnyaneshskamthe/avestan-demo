6166cf8bf5ddedf9f0e19011b53dcbb524dc123c
dnyaneshskamthe <dnyanesh451989@gmail.com>
2023-10-23 11:14:53 +0530

    Tanstack query added in profit&loss page

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,25 @@ import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import "./basic/basic.css"
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     <React.StrictMode>
       <ChakraProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+        
       </ChakraProvider>
     </React.StrictMode>,
 )
--- src/pages/profitLoss/ProfitLoss.jsx
+++ src/pages/profitLoss/ProfitLoss.jsx
@@ -1,7 +1,51 @@
-
+// import { useEffect, useState } from "react";
+import { Box, Image, Heading, Text, Flex } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query'
 
 export default function ProfitLoss() {
+  // const [products, setProducts] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = ("")
+
+  const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/');
+    if (!response.ok) {
+      throw new Error('Response not okay');
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  const {isPending, isError, data, error} = useQuery({ queryKey: ['products'], queryFn: fetchProducts, staleTime:20000 })
+  
+ 
+
+  if(isPending){
+    return <h2>loading...</h2>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
   return (
-    <div>ProfitLoss</div>
-  )
+    <Flex flexWrap="wrap" justifyContent="center">
+      {data && data.map((product) => (
+        <Box
+          key={product.id}
+          borderWidth="1px"
+          borderRadius="md"
+          p="4"
+          maxW="300px"
+          m="2"
+        >
+          <Image src={product.image} alt={product.title} maxW="100%" />
+          <Heading size="md">{product.title}</Heading>
+          <Text fontSize="lg">Price: ${product.price.toFixed(2)}</Text>
+          <Text>{product.description}</Text>
+          <Text>Category: {product.category}</Text>
+        </Box>
+      ))}
+    </Flex>
+  );
 }
